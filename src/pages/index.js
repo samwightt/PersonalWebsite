0a1cea7de5b1c8e6282ee1cabb669211a8d53d7e
samuelwight <samuelwight@gmail.com>
2019-09-15 00:07:04 -0500

    Add exclamation point!

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,15 @@ import SEO from "../components/seo";
 import ProfilePic from '../images/profile-pic.jpg'
 // import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
-function IndexPage() {
+const IndexPage = () => {
+  React.useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) console.log("DARK")
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(e => console.log("HELLO"))
+    window.matchMedia("(prefers-color-scheme: light)").addListener(e => console.log("LIGHT"))
+
+    console.log("HI")
+  })
+
   return (
     <Layout>
       <SEO
@@ -17,13 +25,8 @@ function IndexPage() {
         <img src={ProfilePic}
           className="block mx-auto w-1/3 rounded-full"
           alt="My profile picture" />
-        {/* <img
-          src={catAndHumanIllustration}
-          className="block mx-auto w-1/2"
-          alt="Cat and human sitting on a couch"
-        /> */}
 
-        <h2 className="bg-white text-blue-800 text-2xl font-bold inline-block my-8 p-3">Hi there! I'm Sam Wight</h2>
+        <h2 className="bg-white text-blue-800 text-2xl font-bold inline-block my-8 p-3">Hi there! I'm Sam Wight!</h2>
         <p className="text-blue-900 bg-white text-xl font-semibold mb-4 p-1">I'm a React, Typescript, and Rails dev. I'm also a computer science and applied math major at the University of Alabama.</p>
         <p className="text-blue-900 bg-white text-lg">Feel free to <a href="https://linkedin.com/in/samwightt" className="text-blue-800 font-semibold hover:bg-blue-600 hover:text-white rounded">connect with me on LinkedIn</a>{", "}
           <a href="https://twitter.com/samwightt" className="text-blue-500 font-semibold hover:bg-blue-400 hover:text-white rounded">follow my random ramblings on Twitter</a>{", "}
